613624f84c8e26b5bafcb21e7092d95950a9cba2
vythongvuong <61930417+vythongvuong@users.noreply.github.com>
2023-06-15 16:09:56 +0700

    delete backend source

--- Backend/web_x_backend_net6/web_x_backend_net6/web_x_backend_net6/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace web_x_backend_net6.Models
-{
-    public class TodoItem
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
